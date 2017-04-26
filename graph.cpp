9a46e8a29dd4de6ccedaf90608580a90282b02b4
Daniel Galvao Guerra <dgalvaog@nd.edu>
2017-04-25 21:47:11 -0400

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+static int NUM_CONNECTED = 2;
+
 struct Node{
 
 	int note; //C=60
@@ -23,7 +25,6 @@ class Graph{
 		int size;	
 		Node * current_node; //pointer to current node
 
-
 	public:	
 		
 		Node * getNextNode();
@@ -37,100 +38,67 @@ class Graph{
 
 Graph::Graph(){
 	size = 0;
-
 }
 
 void Graph::addNode(int number){
 	
 	Node temp_node;
 	temp_node.note = number; 
+
 	all_nodes.push_back(temp_node);
+	current_node = &(all_nodes.back());
 
 	size += 1;
-	
 }
 
 
-void Graph::makeAnEdge( Node * first, Node * second){
-	//later: error checking that the indexed node exists
-	//all_nodes[first].edges.push_back(&all_nodes[second]);	
-	
-	first->edges.push_back(second);
-	second->edges.push_back(first);
-	
-
-}
-
-void Graph::makeEdges(){
-	//for now, we are connecting the nodes to all of the other nodes		
-	
-	//listy[first].push_back(second);	
-
-	for(int i = 0; i < size; i++){
-		for(int j = i; j < (size); j++){
-			Node * pointer_first = &all_nodes[i];
-			Node * pointer_second = &all_nodes[j];
-			makeAnEdge(pointer_first, pointer_second);
-		}
+void Graph::makeAnEdge(Node * first, Node * second){
+	if(first->note == second->note)
+	{
+		first->edges.push_back(second);
+	}
+	else
+	{
+		first->edges.push_back(second);
+		second->edges.push_back(first);
 	}
 }
 
+// connect each node to (2) before it and (2) after it
+// algorithm: travel nodes array and connect each node to two itself and two nodes in front of it. Because the
+//  makeAnEdge function conects the nodes both ways, this means nodes get connected to two nodes before them as well
+void Graph::makeEdges(){	
+	for(int i = 0; i < all_nodes.size(); i++)
+	{
+		for(int count=0; count<=NUM_CONNECTED; count++)
+		{
+			if(i+count >= 0 && i+count < all_nodes.size())	// check if index is in bounds
+				makeAnEdge(&all_nodes[i], &all_nodes[i+count]); // connect a node to another node
+		}
+	}
+
+	// for testing: outputs nodes and their edges
+	/*for(int i = 0; i < all_nodes.size(); i ++)
+	{
+		cout << all_nodes[i].note << ":" << all_nodes[i].edges.size() << endl;
+		for(int j=0; j<all_nodes[i].edges.size(); j++)
+			cout << "  " << all_nodes[i].edges[j]->note << ", ";
+		cout << endl;
+	}*/
+}
 
 Node Graph::getNode(int index){
 	return all_nodes[index];
-
 }
 
 int Graph::getSize(){
-
 	return all_nodes.size();
 }
 
+// randomly selects a node connected to the current node
 Node * Graph::getNextNode(){
+	int random = rand() % (current_node->edges.size());	
 
-	//randomly selects the next node
-	
-	//srand(time(NULL));
-
-	int random = rand() % size;	
-	
-	//temp way to pick notes
-	return &all_nodes[random];
-	
-
-}
-
-main(){
-
-srand(time(NULL));
-
-Graph g;
-for(int i = 60; i < 68; i++){
-	g.addNode(i);
-}
-g.makeEdges();
-
-vector<int> notes_in_song;
-
-for(int j = 0; j < 60; j++){
-	Node * pointer = g.getNextNode();
-	int number = pointer->note;
-	notes_in_song.push_back(number);
-}
-/*
-for(int k = 0; k < 50; k++){
-	cout << notes_in_song[k] << endl;
-}
-*/
-
-ofstream ost{"notes.txt"};
-for(int i = 0; i < notes_in_song.size(); i++){
-	int temp;
-	temp = notes_in_song[i];
-	ost << temp << endl << temp << endl << temp << endl;
-
-}
-int sentinel = -1;
-ost << sentinel << endl << sentinel << endl << sentinel << endl;
-//ost << sentinel << endl;	
+	current_node = current_node->edges[random];
+	return current_node;
 }
